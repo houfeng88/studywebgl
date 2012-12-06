ceed16e97f040a9b1c32d180a476be8577e4cea6
houfeng88 <626330965@qq.com>
2012-12-06 16:21:15 +0800

    add studywebgl project

---
+++ StudyWebGL/StudyWebGL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  StudyWebGL
+//
+//  Created by houfeng on 12-12-6.
+//  Copyright (c) 2012年 houfeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
